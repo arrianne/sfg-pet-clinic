bd194aa7354141ea1772d1965bc53569e7f34b11
arrianne <arrianneoshea@hotmail.co.uk>
2019-07-22 11:46:37 +0100

    setting up the main model with the different classes I need - person being the main one

---
+++ src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -0,0 +1,36 @@
+package com.example.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    // property types.
+
+    private petType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public com.example.sfgpetclinic.model.petType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(com.example.sfgpetclinic.model.petType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
